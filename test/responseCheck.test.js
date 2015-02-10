25a5ffb59c74009659fd9bf61df8d355f48f5fae
Nairn McWilliams <n.mcwilliams@ocado.com>
2015-02-10 12:35:20 +0000

    Fix compilation error.

--- test/responseCheck.test.js
+++ test/responseCheck.test.js
@@ -3,5 +3,5 @@ var responseCheck = require('../src/responseCheck').responseCheck;
 var urls = ['http://www.google.co.uk/'].sort();
 
 urls.forEach(function (appUrl) {
-    responseCheck(httpUrl);
+    responseCheck(appUrl);
 });
